e67c1ad7d4ef4d7a7f6b1a248b645efe12127a95
Arkronzxc <ArkronCherf01@gmail.com>
2020-03-20 23:25:09 +0300

    init commit

---
+++ src/test/java/com/github/arkronzxc/ifuture/IfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arkronzxc.ifuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
